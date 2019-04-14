23a1acf92a8be821a660b2e96320aaacc30b2054
unknown <berkelmas96@gmail.com>
2019-04-14 14:21:20 +0300

    direcotr ve onun movie leri tek query ile cekildi

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {gql} from 'apollo-boost';
-import {Query, graphql} from 'react-apollo';
+import {Query} from 'react-apollo';
 
 const sorgu = gql`
 {
@@ -22,10 +22,17 @@ class MovieList extends Component {
               {({loading, data, error}) => {
                 if (loading) return <li>Geliyor knk</li>
                 if (error) return <li>Bir Sorun oluştu knk</li>
-                else return <li>{data.director.name }</li>
+                else return data.director.movies.map((res, index) => <li key={index}>{res.name}</li>)
               }}
             </Query>
           </ul>
+            <Query query={sorgu}>
+            {({loading, data, error}) => {
+              if (loading) return <h2>Getiriom knk</h2>
+              if (error) return <h2>Hata var knk bak bi </h2>
+              else return <h2>Yönetmen Adı: {data.director.name}</h2>
+            }}
+            </Query>
         </div>
       )
   }
